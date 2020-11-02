cdf4d6a3a65ef79aa839970c3fad1dd617f803a7
sebaschr <zebaz9898@gmail.com>
2020-11-02 10:05:26 -0600

    fixed Save

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -16,14 +16,13 @@ export class AdminPanelComponent implements OnInit {
   
   sprintCounter = 0;
   sprintName = 'Sprint ' + this.sprintCounter;
-  minReached = false;
+  minReached = false; 
+  savedLastSprint = true;
   span = <HTMLInputElement>document.getElementById('sprintQuantity');
   sprintList = [];
-  savedSprints = [];
   teamQuantity=1;
   minPlayers=1;
   maxPlayers=2;
-
   form = this.fb.group({
     teamQuantity: ['', Validators.required],
     playerQuantityMin: ['', Validators.required],
@@ -39,9 +38,6 @@ export class AdminPanelComponent implements OnInit {
     retrospectiveTime: ['', Validators.required],
     objectives: ['', Validators.required]
   });
-
-  
-
 
   constructor(public fb: FormBuilder, public dataService: DataService,private router: Router) { 
   
@@ -66,26 +62,17 @@ export class AdminPanelComponent implements OnInit {
     this.router.navigate(['/lobby']);
   }
 
-  // get sprints() {
-  //   // return this.form.get('sprints') as FormArray;
-  // }
+
   saveSprint() {
     this.sprintName = 'Sprint ' + this.sprintCounter;
     let sprint = new Sprint();
-    sprint.name = this.sprintName;
+    sprint.name = this.form.value.selectSprint;
     sprint.ejecucion=this.form.value.executionTime;
     sprint.planeamiento=this.form.value.planningTime;
     sprint.revision=this.form.value.reviewingTime;
     sprint.retrospectiva=this.form.value.retrospectiveTime;
-
-    for(var i=0;i<=this.sprintList.length;i++){
-      if(this.sprintList[i]==sprint.name){
-        this.sprintList[i]=sprint;
-      }
-    }
-    this.savedSprints.push(sprint);
-    this.dataService.session.sprints.push(sprint);//Adds a new sprint to the dataService sprint list.
-    console.log(this.dataService);
+    this.findUpdateSprint(sprint);
+    this.checkLastOneSaved(sprint.name);
   }
 
   removeSprint() {
@@ -94,12 +81,13 @@ export class AdminPanelComponent implements OnInit {
     } else {
       this.sprintCounter -= 1;
       this.sprintName = 'Sprint ' + this.sprintCounter;
-      this.dataService.session.sprints.splice(-1, 1);    //Removes the last sprint on the dataService sprints list.
+      // this.dataService.session.sprints.splice(-1, 1);    //Removes the last sprint on the dataService sprints list.
     }
   }
 
   addSprint(){
     this.minReached=false;
+    this.savedLastSprint = false;
     this.sprintCounter += 1;
     this.sprintName = 'Sprint ' + this.sprintCounter;
     let sprint = new Sprint();
@@ -113,6 +101,56 @@ export class AdminPanelComponent implements OnInit {
     // console.log(this.dataService);
   }
 
+  findUpdateSprint(sprint){
+    //find and update Sprint in SprintList first
+    for(var i=0;i<this.sprintList.length;i++){
+      if(this.sprintList[i].name==sprint.name){
+        this.sprintList[i]=sprint;
+      }
+    }
+    //check if it was saved before
+    if(this.sprintinDataService(sprint.name)==false){
+      //if not push it
+      this.dataService.session.sprints.push(sprint);//Adds a new sprint to the dataService sprint list.
+    }else{
+      //if it exists update it
+      this.updateDataService(sprint);
+    }
+    console.log(this.sprintList)
+    console.log(this.dataService)
+
+
+  }
+
+  checkLastOneSaved(sprintName){
+    var x=this.sprintList.length;
+    if(sprintName==this.sprintList[x-1].name){
+      if(this.sprintinDataService(sprintName)){
+        this.savedLastSprint = true;
+      }
+    }
+  }
+
+  updateDataService(sprint){
+    for(var i=0;i<this.dataService.session.sprints.length;i++){
+      if(this.dataService.session.sprints[i].name==sprint.name){
+        this.dataService.session.sprints[i]=sprint;
+      }
+    }
+  }
+
+  sprintinDataService(sprintName){
+    var exists=false;
+    
+    for(var i=0;i<this.dataService.session.sprints.length;i++){
+      if(this.dataService.session.sprints[i].name==sprintName){
+        exists=true;
+      }else{
+      }
+    }
+    return exists;
+  }
+
   onForm2NameChange({target}){
     var selectedSprint = this.sprintList.find(x => x.name == this.form.value.selectSprint);
     this.form.controls['executionTime'].setValue(selectedSprint.ejecucion);
@@ -121,19 +159,9 @@ export class AdminPanelComponent implements OnInit {
     this.form.controls['retrospectiveTime'].setValue(selectedSprint.retrospectiva);
   }
 
-  // updateSprintFormControl() {
-  //   //TODO: clean all controls from the sprints FormArray.
-
-  //   //TODO: Loop thru all sprints in dataService and create new form controls.
-
-  //   // this.sprints.push(this.fb.control(this.sprintName));
-  // }
 
   validateFields() {
 
   }
 
-  owo(){
-    console.log('owo')
-  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -36,6 +36,6 @@ export class DataService {
     //console.log(name);
   }
 
-  savePlayerToTeam ()
+  savePlayerToTeam (){}
 
 }
