f3327ffaae75b9be04011841b49bade2b0513b2c
sebaschr <zebaz9898@gmail.com>
2020-11-02 01:20:27 -0600

    arreglos
    - [x] Boton de Borrar Sprint
    - [x] Boton de Guardar Sprint
    - [x] Player Quantity (Min set en 1 default) (Max 2 default)
    - [x] Select Sprint Dinamico
    - [x] Boton de Agregar Sprint
    - [x] Sprint - Cada Sprint debe actualizarse con sus propios datos
    - [x] Sprint - Se salva en dataservice
    - [x] Start - Redireccionar al Lobby

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { DataService } from '../data.service';
 import { ACSession } from '../models/acSession';
@@ -13,12 +13,17 @@ import { Router } from '@angular/router';
 })
 
 export class AdminPanelComponent implements OnInit {
-
+  
   sprintCounter = 0;
   sprintName = 'Sprint ' + this.sprintCounter;
   minReached = false;
   span = <HTMLInputElement>document.getElementById('sprintQuantity');
-  
+  sprintList = [];
+  savedSprints = [];
+  teamQuantity=1;
+  minPlayers=1;
+  maxPlayers=2;
+
   form = this.fb.group({
     teamQuantity: ['', Validators.required],
     playerQuantityMin: ['', Validators.required],
@@ -35,23 +40,52 @@ export class AdminPanelComponent implements OnInit {
     objectives: ['', Validators.required]
   });
 
-  constructor(public fb: FormBuilder, public dataService: DataService,private router: Router) { }
+  
+
+
+  constructor(public fb: FormBuilder, public dataService: DataService,private router: Router) { 
+  
+  }
 
   ngOnInit(): void {
 
   }
 
   submit() {
-    console.log(this.form.value);
+    
+  }
+
+  submitInfo(){
+    this.dataService.session.objectives=this.form.value.objectives;
+    this.dataService.session.playersMin=this.form.value.playerQuantityMin;
+    this.dataService.session.playersMax=this.form.value.playerQuantityMax;
+    this.dataService.session.teams.length=this.form.value.teamQuantity;
     this.dataService.saveSessionToLocalStorage(new ACSession());
-    // this.router.navigate(['/lobby']);
+    console.log(this.dataService);
+
+    this.router.navigate(['/lobby']);
   }
 
   // get sprints() {
   //   // return this.form.get('sprints') as FormArray;
   // }
-  addSprint() {
-    
+  saveSprint() {
+    this.sprintName = 'Sprint ' + this.sprintCounter;
+    let sprint = new Sprint();
+    sprint.name = this.sprintName;
+    sprint.ejecucion=this.form.value.executionTime;
+    sprint.planeamiento=this.form.value.planningTime;
+    sprint.revision=this.form.value.reviewingTime;
+    sprint.retrospectiva=this.form.value.retrospectiveTime;
+
+    for(var i=0;i<=this.sprintList.length;i++){
+      if(this.sprintList[i]==sprint.name){
+        this.sprintList[i]=sprint;
+      }
+    }
+    this.savedSprints.push(sprint);
+    this.dataService.session.sprints.push(sprint);//Adds a new sprint to the dataService sprint list.
+    console.log(this.dataService);
   }
 
   removeSprint() {
@@ -61,22 +95,30 @@ export class AdminPanelComponent implements OnInit {
       this.sprintCounter -= 1;
       this.sprintName = 'Sprint ' + this.sprintCounter;
       this.dataService.session.sprints.splice(-1, 1);    //Removes the last sprint on the dataService sprints list.
-      // this.updateSprintFormControl();
     }
   }
 
-  saveSprint(){
+  addSprint(){
     this.minReached=false;
     this.sprintCounter += 1;
     this.sprintName = 'Sprint ' + this.sprintCounter;
     let sprint = new Sprint();
     sprint.name = this.sprintName;
-    sprint.ejecucion=this.form.value.executionTime;
-    sprint.planeamiento=this.form.value.planningTime;
-    sprint.revision=this.form.value.reviewingTime;
-    sprint.retrospectiva=this.form.value.retrospectiveTime;
-    this.dataService.session.sprints.push(sprint);//Adds a new sprint to the dataService sprint list.
-    console.log(this.dataService);
+    sprint.ejecucion=45;
+    sprint.planeamiento=45;
+    sprint.revision=45;
+    sprint.retrospectiva=45;
+    this.sprintList.push(sprint);    
+    // this.dataService.session.sprints.push(sprint);//Adds a new sprint to the dataService sprint list.
+    // console.log(this.dataService);
+  }
+
+  onForm2NameChange({target}){
+    var selectedSprint = this.sprintList.find(x => x.name == this.form.value.selectSprint);
+    this.form.controls['executionTime'].setValue(selectedSprint.ejecucion);
+    this.form.controls['planningTime'].setValue(selectedSprint.planeamiento);
+    this.form.controls['reviewingTime'].setValue(selectedSprint.revision);
+    this.form.controls['retrospectiveTime'].setValue(selectedSprint.retrospectiva);
   }
 
   // updateSprintFormControl() {
@@ -91,11 +133,7 @@ export class AdminPanelComponent implements OnInit {
 
   }
 
-  //TODO: Update the data structure on the dataService
-
-  //TODO: 3. Change the Sprint Quantity input fot a just text
-  //TODO: 4: Update the select with the sprints created.
-
-  //TODO: 
-
+  owo(){
+    console.log('owo')
+  }
 }
--- src/app/models/acSession.ts
+++ src/app/models/acSession.ts
@@ -12,6 +12,7 @@ export class ACSession {
 
     public playersMin = 0;
     public playersMax = 0;
+    public objectives='';
     public teams: { players: [] }[] = [];
     public sprints: { name: string, planeamiento: number, ejecucion: number, revision: number, retrospectiva: number }[] = [];
 
