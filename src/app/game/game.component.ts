3fd282333ff8adfc5a7e56fa8d637a10aa25887f
sebaschr <zebaz9898@gmail.com>
2020-11-09 21:17:47 -0600

    images, game
    missing data structure

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ThemePalette } from '@angular/material/core';
+import { element } from 'protractor';
 
 
 interface Overlay {
@@ -28,7 +29,7 @@ export class GameComponent implements OnInit {
       progress: 0,
       width: '100px',
       height: '100px',
-      onpizza:'block',
+      onpizza: 'block',
       position: [{
       }]
     },
@@ -40,9 +41,59 @@ export class GameComponent implements OnInit {
       progress: 0,
       width: '100px',
       height: '100px',
-      onPizza:'block',
+      onPizza: 'block',
       position: [{
 
+      }]
+    },
+    {
+      name: 'Pepper',
+      img2: '../../assets/pepper_1.png',
+      img3: '../../assets/pepper_2.png',
+      activeImg: '../../assets/tomato_1.png',
+      progress: 0,
+      width: '100px',
+      height: '100px',
+      onPizza: 'block',
+      position: [{
+      }]
+    },
+    {
+      name: 'Mushroom',
+      img2: '../../assets/shroom_2.png',
+      img3: '../../assets/shroom_3.png',
+      activeImg: '../../assets/shroom_1.png',
+      progress: 0,
+      width: '100px',
+      height: '100px',
+      onPizza: 'block',
+      position: [{
+
+      }]
+    },
+    {
+      name: 'Pepperoni',
+      img2: '../../assets/pepperoni_1.png',
+      img3: '../../assets/pepperoni_2.png',
+      activeImg: '../../assets/pepperoni_2.png',
+      progress: 0,
+      width: '100px',
+      height: '100px',
+      onPizza: 'block',
+      position: [{
+
+      }]
+    },
+    {
+      name: 'Cheese',
+      activeImg: 'https://png.pngtree.com/element_our/png_detail/20181227/cheese-vector-icon-png_293618.jpg',
+      img2: 'https://media.istockphoto.com/vectors/grating-cheese-icon-flat-vector-id843941538?b=1&k=6&m=843941538&s=612x612&w=0&h=BI4SDehRI_lvIkea3FCZCnwPjN4P21flAYkScTiHnvY=',
+      img3: 'https://icon2.cleanpng.com/20180202/ide/kisspng-pizza-grated-cheese-processed-cheese-cheese-png-photos-5a74976e7d0ab7.1754448615175903825122.jpg',
+      progress: 0,
+      width: '100px',
+      height: '100px',
+      onpizza: 'block',
+      position: [{
       }]
     }];
 
@@ -81,7 +132,7 @@ export class GameComponent implements OnInit {
       { name: 'Tomato', amount: 2 }
     ],
     editing: false
-  }, {
+    }, {
     pizzaID: 'pizza03',
     name: 'Pepperoni',
     imgSrc: '../../assets/pepperoni_pizza.png',
@@ -89,7 +140,17 @@ export class GameComponent implements OnInit {
       { name: 'Dough', amount: 1 },
       { name: 'Tomato', amount: 3 }
     ],
-    editing: false
+    editing: false,
+  },
+  {
+    pizzaID: 'pizza03',
+    name: 'Pepperoni',
+    imgSrc: '../../assets/pepperoni_pizza.png',
+    ingredients: [
+      { name: 'Dough', amount: 1 },
+      { name: 'Tomato', amount: 3 }
+    ],
+    editing: false,
   }]
 
   inProduction = [];
@@ -148,7 +209,7 @@ export class GameComponent implements OnInit {
 
   prepareIngredient(ing, e: MouseEvent) {
     var count = e.detail;
-    var ingredientDiv = document.querySelector('.ingredient');
+    var ingredientDiv = e.currentTarget;
     var checkInside = this.checkifinsideofCanvas(ingredientDiv);
 
 
@@ -197,6 +258,10 @@ export class GameComponent implements OnInit {
     return inside;
   }
 
+  checkiffinished() {
+
+  }
+
   assignIngredientstoPlayers(ing) {
 
     var ingredients = [];
@@ -211,21 +276,65 @@ export class GameComponent implements OnInit {
   }
 
   mixIngredient(ing1, ing2) {
+
+    this.checkifInsideofTrash(ing1);
+    this.checkifInsideofOven(ing1);
+    this.checkifInsideofFinishedQueue(ing1);
+
     if (ing1.progress < 100 || ing2.progress < 100) {
     } else {
       if ((ing1.name == 'Dough' || ing2.name == 'Dough') && (ing1.name == 'Tomato' || ing2.name == 'Tomato')) {
-
-
         var checkifInside = this.checkifInside(ing1, ing2);
         if (checkifInside) {
-          ing2.activeImg='https://ih1.redbubble.net/image.1054548113.6625/poster,840x830,f8f8f8-pad,1000x1000,f8f8f8.u1.jpg'
-          ing1.onPizza='none';
+          ing2.activeImg = '../../assets/pizza_sauce.png';
+          ing1.onPizza = 'none';
+          ing2.name = 'PizzaSauce';
         } else {
         }
       }
+
+      if ((ing1.name == 'PizzaSauce' || ing2.name == 'PizzaSauce') && (ing1.name == 'Cheese' || ing2.name == 'Cheese')) {
+        var checkifInside = this.checkifInside(ing1, ing2);
+        if (checkifInside) {
+          ing2.activeImg = '../../assets/pizza_cheese.png';
+          ing1.onPizza = 'none';
+          ing2.name = 'PizzaSauceCheese';
+          
+        } else {
+        }
+      }
+
+      // Pizza Flavours
+
+      if ((ing1.name == 'PizzaSauceCheese' || ing2.name == 'PizzaSauceCheese') && (ing1.name == 'Mushroom' || ing2.name == 'Mushroom')) {
+        var checkifInside = this.checkifInside(ing1, ing2);
+        if (checkifInside) {
+          ing2.activeImg = '../../assets/mushroom_pizza.png';
+          ing1.onPizza = 'none';
+          ing2.progress = 0;
+          ing2.width='100px';
+          ing2.height='100px';
+        } else {
+        }
+      }
+
+      if ((ing1.name == 'PizzaSauceCheese' || ing2.name == 'PizzaSauceCheese') && (ing1.name == 'Pepperoni' || ing2.name == 'Pepperoni')) {
+        var checkifInside = this.checkifInside(ing1, ing2);
+        if (checkifInside) {
+          ing2.activeImg = '../../assets/pepperoni_pizza.png';
+          ing1.onPizza = 'none';
+          ing2.progress = 0;
+          ing2.width='100px';
+          ing2.height='100px';
+
+        } else {
+        }
+      }
+
+
     }
   }
-
+  //Check if ingredient is inside 
   checkifInside(pos1, pos2) {
     var inside = false;
     var position1 = pos1.position;
@@ -238,6 +347,95 @@ export class GameComponent implements OnInit {
       }
     }
     return inside;
+  }
+  //Throw it in the garbage
+  checkifInsideofTrash(pos1) {
+
+    var inside = false;
+
+    var trashDiv = document.querySelector('.trash');
+
+    var trashPosition = trashDiv.getBoundingClientRect();
+
+    var inside = false;
+    var position1 = pos1.position;
+    var position2 = trashPosition;
+
+
+    if ((position1.right < position2.right) && (position2.left < position1.left)) {
+      if ((position1.bottom < position2.bottom) && (position1.top > position2.top)) {
+        inside = true;
+      }
+    }
+
+    if (inside) {
+      pos1.onPizza = 'none';
+    }
+  }
+
+  checkifInsideofOven(pos1) {
+
+    var inside = false;
+
+    var ovenDiv = document.querySelector('.oven');
+
+    var ovenPosition = ovenDiv.getBoundingClientRect();
+
+    var inside = false;
+    var position1 = pos1.position;
+    var position2 = ovenPosition;
+
+
+    if ((position1.right < position2.right) && (position2.left < position1.left)) {
+      if ((position1.bottom < position2.bottom) && (position1.top > position2.top)) {
+        inside = true;        
+      }
+    }
+
+    if(inside && pos1.name == 'PizzaSauceCheese'){
+      this.countUp(100,5000,pos1)
+    }
+  }
+
+  checkifInsideofFinishedQueue(pos1){
+    var inside = false;
+
+    var readyQueueDiv = document.querySelector('.readyQueue');
+
+    var readyQueuePosition = readyQueueDiv.getBoundingClientRect();
+
+    var inside = false;
+    var position1 = pos1.position;
+    var position2 = readyQueuePosition;
+
+
+    if ((position1.right < position2.right) && (position2.left < position1.left)) {
+      if ((position1.bottom < position2.bottom) && (position1.top > position2.top)) {
+        inside = true;        
+        console.log(pos1.progress)
+      }
+    }
+
+    if(inside && pos1.name == 'PizzaSauceCheese' && pos1.progress>=100){
+      pos1.finished=true;
+      console.log('yo')
+    }
+  }
+
+  countUp (max, time,p) {
+    var num = 0;
+    var step = time / max; // calculate the time between two steps of counting
+    // create an inner function that performs one step of counting
+    var fn = function () {
+      p.progress++;
+      if (num <= max) {
+        // ... and call the inner function again, some time in the future
+        window.setTimeout(fn, step);
+      }
+    }
+    
+    // call the inner function for the first time
+    fn();
   }
 }
 
