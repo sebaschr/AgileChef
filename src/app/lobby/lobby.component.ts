959062735f3daff9b49a2c87b472d9769a2a0b25
sebaschr <zebaz9898@gmail.com>
2020-11-10 21:43:59 -0600

    arrayTEam

--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Router } from '@angular/router';
+import { Team } from '../models/team';
+import { templateSourceUrl } from '@angular/compiler';
 
 
 @Component({
@@ -11,10 +13,11 @@ import { Router } from '@angular/router';
 
 
 export class LobbyComponent implements OnInit {
-   teams  = [];
+    
+  teams: Team [] = [];
 
   constructor(public dataService: DataService, private router: Router) {
-
+  
    if (typeof (dataService.currentPlayer && dataService.session) == 'undefined') {
       dataService.loadPlayerFromLocalStorage();
       dataService.loadSessionFromLocalStorage();
@@ -24,8 +27,7 @@ export class LobbyComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.dataService.loadSessionFromLocalStorage().teams);
-    this.teams = this.dataService.loadSessionFromLocalStorage().teams;
+      this.teams=this.dataService.loadSessionFromLocalStorage().teams;
   }
 
   startGame() {
